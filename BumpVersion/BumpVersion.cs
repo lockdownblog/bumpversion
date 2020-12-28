ff72182334c39e8f3efaf1017db89b5f19e89cb3
Antonio Feregrino <antonio.feregrino@gmail.com>
2020-12-28 09:11:51 +0000

    Implement command line utils

---
+++ BumpVersion/BumpVersion.cs
@@ -0,0 +1,23 @@
+﻿namespace BumpVersion
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using McMaster.Extensions.CommandLineUtils;
+
+    public class BumpVersion
+    {
+        [Argument(0)]
+        [Required]
+        public string Part { get; }
+
+        public static int Main(string[] args)
+        {
+            return CommandLineApplication.Execute<BumpVersion>(args);
+        }
+
+        private void OnExecute()
+        {
+            Console.WriteLine($"Hello! you will bump \"{this.Part}\".");
+        }
+    }
+}
