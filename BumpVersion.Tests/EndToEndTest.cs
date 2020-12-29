a472f7e895d7cd0ab3693dfcd948aa71d28951b4
Antonio Feregrino <antonio.feregrino@gmail.com>
2020-12-29 08:13:07 +0000

    wip

--- BumpVersion.Tests/EndToEndTest.cs
+++ BumpVersion.Tests/EndToEndTest.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using Docker.DotNet;
 
 namespace BumpVersion.Tests
 {
@@ -8,6 +9,14 @@ namespace BumpVersion.Tests
         [Fact]
         public void Test1()
         {
+            /**
+             DockerClient client = new DockerClientConfiguration(
+    new Uri("npipe://./pipe/docker_engine"))
+     .CreateClient();
+            */
+            DockerClient client = new DockerClientConfiguration(
+       new Uri("unix:///var/run/docker.sock"))
+        .CreateClient();
 
         }
     }
